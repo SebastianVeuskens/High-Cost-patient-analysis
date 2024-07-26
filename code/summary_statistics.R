80cb0740b08c332b7771e4771e190934f61684da
Sebastian Veuskens <sebastian.veuskens@googlemail.com>
2024-07-26 13:18:28 +0200

    Add summary statistic function & requirements

--- code/summary_statistics.R
+++ code/summary_statistics.R
@@ -12,13 +12,15 @@
 
 #### MODIFY ####
 # Your working directory 
-setwd('C:/Users/s.veuskens/Documents/Sebastian/Projekt Sebastian/modelling')
+setwd("C:/Users/Sebastian's work/OneDrive - OptiMedis AG/Dokumente/Coding/High-Cost-patient-analysis")
 # Indicates whether to include High-Cost patients from the last year into analysis 
 filter_hc <- FALSE 
 # Indicates whether to include as many High-Cost patients as not-High-Cost patients 
 balance_hc <- FALSE 
 # Number of the best models to save in the best parameters folder 
 num_models <- 2 
+# Way to divide the age into groups
+age_split <- c(30, 65, 80)
 #### MODIFY END ####
 
 #######################
@@ -28,10 +30,14 @@ num_models <- 2
 #### LIBRARIES & SOURCES ####
 
 # INSTALL LIBRARIES 
+# install.packages('tidyr')
+# install.packages('dplyr')
 # install.packages('arsenal')
 
 # LOAD LIBRARIES & SOURCES
 source('code/utils.R')  # Auxiliary functions for simplicity and concise code 
+library('tidyr')        # Auxiliary tools for frequency tables 
+library('dplyr')        # Auxiliary tools for frequency tables  
 library('arsenal')      # Package for summary statistics 
 
 #### LOAD DATA ####
@@ -53,18 +59,29 @@ label_pos <- 1
 first_val <- 2
 last_val <- ncol(train)
 
+#### CREATE FOLDER STRUCTURE ####
+if (overwrite) {
+    dir.create('results', showWarnings=FALSE)
+    dir.create(paste0('results/', ifelse(filter_hc, 'filtered', 'complete')), showWarnings=FALSE)
+    dir.create(paste0('results/', relative_dir), showWarnings=FALSE)
+    dir.create(paste0('results/', relative_dir, 'summary_statistics'), showWarnings=FALSE)
+}
+
 #######################
 #### SUMMARIZE ########
 #######################
 cur_data <- data_2019
-
-n <- nrow(cur_data)
-str(cur_data)
+cur_data$Age_groups_num <- findInterval(cur_data$Age, age_split)
+# TODO: Make this dynamic so that it changes when the group change in the modify part 
+age_code <- c('0-30' = 0, '30-65' = 1, '65-80' = 2, '>80' = 3)
+cur_data$Age_groups <- names(age_code)[match(cur_data$Age_groups_num, age_code)]
+# cur_data <- as.data.frame(cbind(c(1, 2, 3), c(4, 5, 6), c(7, 8, 9), c(10, 11, 12)))
 
 variables <- c('HC_Patient_Next_Year'
-             , 'Total_Costs_Next_Year',
+             , 'Total_Costs_Next_Year'
              , 'HC_Patient'
-             , 'Age'
+             , 'Sex'
+             , 'Age_groups'
              , 'Need_of_Care_Duration'
              , 'DMP_Duration'
              , 'Total_Costs'
@@ -73,7 +90,28 @@ variables <- c('HC_Patient_Next_Year'
              , 'Prescription_Num_Prescriptions'
                 )
 
-gather(hospdata[,variables], "var", "value", -race) %>%
-  count(var, value) %>%
-  group_by(var) %>%       
-  mutate(prop = prop.table(n))
+cur_data <- cur_data[,variables]        
+n <- nrow(cur_data)
+str(cur_data)
+
+
+
+variable_frequency <- function(data, variable) {
+  return(
+    gather(data, 'var', 'value', variable) %>%
+      count(var, value) %>%
+      group_by(var) %>%       
+      mutate(prop = prop.table(n))
+  )
+}
+frequency_table <- list(
+  'Sex' = variable_frequency(cur_data, 'Sex'),
+  'Age' = variable_frequency(cur_data, 'Age_groups'),
+  'HC_Patient' = variable_frequency(cur_data, 'HC_Patient')
+)
+print(frequency_table)
+
+## Save frequency table 
+rf_filepath <- paste0('results/', relative_dir, 'summary_statistics/frequency_table')
+if (overwrite) save_list(frequency_table, rf_filepath)
+
