9eb6e2dd09142b50241a0f1664085c938a070c29
Sebastian Veuskens <sebastian.veuskens@googlemail.com>
2024-07-30 15:13:00 +0200

    Add cost capture

--- code/utils.R
+++ code/utils.R
@@ -37,14 +37,7 @@ confidence_interval <- function(value, n, confidence = 0.95) {
 
 # Compute the AUC -> Used in evaluate_model
 # TODO: Add documentation here 
-compute_auc <- function(model, newdata, label, confidence = 0.95) {
-    if (is.null(newdata)) {
-        predictions <- h2o.cross_validation_holdout_predictions(model)
-        newdata <- h2o.getFrame(model@parameters$training_frame)        # Gets the training data that was used for training the model 
-    } else {
-        predictions <- h2o.predict(model, newdata=newdata)
-    }
-    predictions <- as.data.frame(predictions)
+compute_auc <- function(predictions, newdata, label, confidence = 0.95) {
     labels <- as.data.frame(newdata[, label])[[1]]
     probabilities <- predictions$p1
     auc_info <- ci.cvAUC(probabilities, labels, confidence=confidence)
@@ -59,7 +52,7 @@ compute_auc <- function(model, newdata, label, confidence = 0.95) {
 # @params model The trained model
 # @params filepath The filepath and name to save the results 
 # @params newdata The test data to evaluate the model on. If NULL, cross validation results are used
-evaluate_model <- function(model, filepath, overwrite, newdata = NULL, label='HC_Patient_Next_Year'){
+evaluate_model <- function(model, filepath, overwrite, newdata = NULL, target_label='HC_Patient_Next_Year', cost_label='Total_Costs_Next_Year'){
     # Use the G-Mean score of sensitivity and specificity to determine the optimal cutoff threshold 
     # xval indicates that we want to receive the results of cross-validation -> Use only if no newdata is provided
     xval = is.null(newdata)
@@ -70,31 +63,46 @@ evaluate_model <- function(model, filepath, overwrite, newdata = NULL, label='HC
     threshold_index <- which.max(gmeans)
     threshold       <- sensitivities[threshold_index, 'threshold'] # Could also use specificities, the thresholds are the same 
 
+    # Compute predictions 
+    if (is.null(newdata)) {
+        predictions <- h2o.cross_validation_holdout_predictions(model)
+        newdata <- h2o.getFrame(model@parameters$training_frame)        # Gets the training data that was used for training the model 
+    } else {
+        predictions <- h2o.predict(model, newdata=newdata)
+    }
+    predictions <- as.data.frame(predictions)
+    a <- predictions
+
     # Compute the important measures with this threshold 
     confusion_matrix <- h2o.confusionMatrix(performance, threshold=threshold)
     accuracy         <-  as.numeric(h2o.accuracy     (performance, threshold))       
     sensitivity      <-  as.numeric(h2o.sensitivity  (performance, threshold)) 
     specificity      <-  as.numeric(h2o.specificity  (performance, threshold)) 
-    gmean            <- gmeans[threshold_index]                       
+    gmean            <- gmeans[threshold_index]       
+
+
+    idx_cc_pred      <- which(predictions$predict == 1)
+    idx_cc_true      <- which(as.data.frame(newdata)[target_label] == 1)
+    cost_capture     <- 100 * sum(newdata[idx_cc_pred, cost_label]) / sum(newdata[idx_cc_true,cost_label])  
 
     # Confidence intervals 
-    # TODO: Find way to specify n if newdata is NULL 
-    n <- if(is.null(newdata)) NULL else nrow(newdata)
-    interval_accuracy    <- confidence_interval(accuracy, n)
-    interval_sensitivity <- confidence_interval(sensitivity, n)
-    interval_specificity <- confidence_interval(specificity, n)
-    interval_gmean       <- confidence_interval(gmean, n)
+    n <- nrow(newdata)
+    interval_accuracy       <- confidence_interval(accuracy, n)
+    interval_sensitivity    <- confidence_interval(sensitivity, n)
+    interval_specificity    <- confidence_interval(specificity, n)
+    interval_gmean          <- confidence_interval(gmean, n)
+    interval_cost_capture   <- confidence_interval(cost_capture * 0.01, n) * 100
 
     # AUC 
-    auc_info     <- compute_auc(model, newdata=newdata, label=label)
+    auc_info     <- compute_auc(predictions, newdata=newdata, label=target_label)
     auc          <- auc_info[['cvAUC']]
     interval_auc <- auc_info[['ci']]
 
     # Combine the results 
-    measures <- c('accuracy', 'sensitivity', 'specificity', 'gmean', 'auc')
-    values   <- c(accuracy, sensitivity, specificity, gmean, auc) 
-    lower    <- c(interval_accuracy[1], interval_sensitivity[1], interval_specificity[1], interval_gmean[1], interval_auc[1])
-    upper    <- c(interval_accuracy[2], interval_sensitivity[2], interval_specificity[2], interval_gmean[2], interval_auc[2])
+    measures <- c('accuracy', 'sensitivity', 'specificity', 'gmean', 'auc', 'cost_capture')
+    values   <- c(accuracy, sensitivity, specificity, gmean, auc, cost_capture) 
+    lower    <- c(interval_accuracy[1], interval_sensitivity[1], interval_specificity[1], interval_gmean[1], interval_auc[1], interval_cost_capture[1])
+    upper    <- c(interval_accuracy[2], interval_sensitivity[2], interval_specificity[2], interval_gmean[2], interval_auc[2], interval_cost_capture[2])
     results  <- data.frame(measures, values, lower, upper)
 
     # Save the results 
