f0205f6b5732b03c0c9c845c0ffab3268684442e
Sebastian Veuskens <s.veuskens@optimedis.de>
2024-09-04 16:25:42 +0200

    Select methods, add runtimes & figure saving functionalities

--- code/5_explanation_dalex.R
+++ code/5_explanation_dalex.R
@@ -29,9 +29,14 @@ use_pos <- TRUE
 # Whether to use a patient that is predicted positive or negative
 use_true <- TRUE
 # Specify the number of features you want to include in the LIME surrogate model  
-n_features <- 2
+n_features_lime <- 5
 # Specify which feature to investigate better 
 feature_of_interest <- 'Age'
+# TODO: Change this in later evaluations 
+# sample_indices <- c(1, 1467, 2538, 33839)
+sample_indices <- 1467
+# Variable to be used for grouping by grouped PDP plot
+feature_to_group <- 'Total_Costs' # Must be categorical variable 
 #### MODIFY END ####
 
 #######################
@@ -125,35 +130,40 @@ predictions <- as.numeric(prediction_probs >= model$cutoff)
 #### PRELIMINARIES ####
 #######################
 
+# Specifiy where to save the results
+result_filepath <- paste0('results/', relative_dir, 'model_explanation/dalex/')
+
 # Select samples
 test_df <- as.data.frame(test)
 train_validate_df <- as.data.frame(train_validate)
 test_df$HC_Patient_Next_Year <- as.numeric(test_df$HC_Patient_Next_Year) - 1
 train_validate_df$HC_Patient_Next_Year <- as.numeric(train_validate_df$HC_Patient_Next_Year) - 1
 
-true_pos <- test_df[test_df$HC_Patient_Next_Year == 1 & predictions == 1,]
-false_pos <- test_df[test_df$HC_Patient_Next_Year == 1 & predictions == 0,]
-true_neg <- test_df[test_df$HC_Patient_Next_Year == 0 & predictions == 0,]
-false_neg <- test_df[test_df$HC_Patient_Next_Year == 0 & predictions == 1,]
+# true_pos <- test_df[test_df$HC_Patient_Next_Year == 1 & predictions == 1,]
+# false_pos <- test_df[test_df$HC_Patient_Next_Year == 1 & predictions == 0,]
+# true_neg <- test_df[test_df$HC_Patient_Next_Year == 0 & predictions == 0,]
+# false_neg <- test_df[test_df$HC_Patient_Next_Year == 0 & predictions == 1,]
 
-nrow(true_pos)
-nrow(false_pos)
-nrow(true_neg)
-nrow(false_neg)
+# nrow(true_pos)
+# nrow(false_pos)
+# nrow(true_neg)
+# nrow(false_neg)
 
-if (use_pos) {
-    if (use_true) {
-        sample <- true_pos[1,]
-    } else {
-        sample <- false_pos[1,]
-    }
-} else {
-    if (use_true) {
-        sample <- true_neg[1,]
-    } else {
-        sample <- false_neg[1,]
-    }
-}
+# if (use_pos) {
+#     if (use_true) {
+#         sample <- true_pos[1,]
+#     } else {
+#         sample <- false_pos[1,]
+#     }
+# } else {
+#     if (use_true) {
+#         sample <- true_neg[1,]
+#     } else {
+#         sample <- false_neg[1,]
+#     }
+# }
+
+sample <- test_df[sample_indices,]
 
 # TODO: Add part for logistic regresion later 
 if (user_model_name == 'logistic regression') {
@@ -178,34 +188,38 @@ vi_dalex <- model_parts(explainer=exp_dalex,
                         B=1,
                         type='difference')
 
+if (overwrite) {png(paste0(result_filepath, 'variable_importance.png'))}
 plot(vi_dalex)
+if (overwrite) {dev.off()}
 
 # Stop and report time
 vi_end <- Sys.time()
-vi_runtim <- difftime(vi_end, vi_start, units='mins')
-print(paste0('Runtime for break-down plots: ', round(vi_runtime, 2), ' minutes'))
+vi_runtime <- difftime(vi_end, vi_start, units='mins')
+print(paste0('Runtime for variable importance plots: ', round(vi_runtime, 2), ' minutes'))
 
 ##############
 #### SHAP ####
 ##############
 
 # Start time measurement 
-shap_start <- Sys.time()
+# shap_start <- Sys.time()
 
-shap_dalex <- predict_parts(explainer=exp_dalex, 
-                            new_observation=sample, 
-                            type='shap',
-                            B=25 # Number of orderings of explanatory variables to compute the shapley values 
-                           )
+# shap_dalex <- predict_parts(explainer=exp_dalex, 
+#                             new_observation=sample, 
+#                             type='shap',
+#                             B=25 # Number of orderings of explanatory variables to compute the shapley values 
+#                            )
 
-# EXPLANATION: With boxplots, one can see whether effects are reliable.
-# If, for example, boxplot goes over zero line, effect could also be negative with statistical tolerance 
-plot(shap_dalex, show_boxplots=TRUE)   
+# # EXPLANATION: With boxplots, one can see whether effects are reliable.
+# # If, for example, boxplot goes over zero line, effect could also be negative with statistical tolerance 
+# if (overwrite) {png(paste0(result_filepath, 'shap_local_analysis.png'))}
+# plot(shap_dalex, show_boxplots=TRUE)   
+# if (overwrite) {dev.off()}
 
-# Stop and report time
-shap_end <- Sys.time()
-shap_runtim <- difftime(shap_end, shap_start, units='mins')
-print(paste0('Runtime for SHAP plots: ', round(shap_runtime, 2), ' minutes'))
+# # Stop and report time
+# shap_end <- Sys.time()
+# shap_runtime <- difftime(shap_end, shap_start, units='mins')
+# print(paste0('Runtime for SHAP plots: ', round(shap_runtime, 2), ' minutes'))
 
 
 ##############
@@ -226,11 +240,14 @@ lime_dalex <- predict_surrogate(explainer=exp_dalex,
                                 n_features=n_features_lime,
                                 n_permutations=1000,
                                 type='lime')
+
+if (overwrite) {png(paste0(result_filepath, n_features_lime, '_features_lime_plot.png'), width=1400, height=750)}
 plot(lime_dalex)
+if (overwrite) {dev.off()}
 
 # Stop and report time
 lime_end <- Sys.time()
-lime_runtim <- difftime(lime_end, lime_start, units='mins')
+lime_runtime <- difftime(lime_end, lime_start, units='mins')
 print(paste0('Runtime for LIME plots: ', round(lime_runtime, 2), ' minutes'))
 
 
@@ -241,21 +258,21 @@ print(paste0('Runtime for LIME plots: ', round(lime_runtime, 2), ' minutes'))
 # somehow similar to Counterfactual explanations 
 
 # Start time measurement 
-locModel_start <- Sys.time()
+# locModel_start <- Sys.time()
 
-# Also uses LIME. 
-locModel_dalex <- predict_surrogate(explainer=exp_dalex,
-                                    new_observation=sample,
-                                    size=1000,
-                                    seed=1,
-                                    type='localModel')
+# # Also uses LIME. 
+# locModel_dalex <- predict_surrogate(explainer=exp_dalex,
+#                                     new_observation=sample,
+#                                     size=1000,
+#                                     seed=1,
+#                                     type='localModel')
 
-plot_interpretable_feature(locModel_dalex, feature_of_interest)                                    
+# plot_interpretable_feature(locModel_dalex, feature_of_interest)                                    
 
-# Stop and report time
-locModel_end <- Sys.time()
-locModel_runtim <- difftime(locModel_end, locModel_start, units='mins')
-print(paste0('Runtime for local model plots: ', round(locModel_runtime, 2), ' minutes'))
+# # Stop and report time
+# locModel_end <- Sys.time()
+# locModel_runtime <- difftime(locModel_end, locModel_start, units='mins')
+# print(paste0('Runtime for local model plots: ', round(locModel_runtime, 2), ' minutes'))
 
 
 ################################
@@ -265,27 +282,27 @@ print(paste0('Runtime for local model plots: ', round(locModel_runtime, 2), ' mi
 # Helpful to compare to other users and determine model reliability in that certain region. 
 
 # Start time measurement 
-locDiag_start <- Sys.time()
+# locDiag_start <- Sys.time()
 
-neighbors <- 100
-locDiag_dalex_gen <- DALEX::predict_diagnostics(explainer=exp_dalex,
-                                         new_observation=sample,
-                                         neighbors=neighbors)
+# neighbors <- 100
+# locDiag_dalex_gen <- DALEX::predict_diagnostics(explainer=exp_dalex,
+#                                          new_observation=sample,
+#                                          neighbors=neighbors)
 
-plot(locDiag_dalex_gen)
+# plot(locDiag_dalex_gen)
 
-neighbors <- 10
-locDiag_dalex_ind <- predict_diagnostics(explainer=exp_dalex,
-                                         new_observation=sample,
-                                         neighbors=neighbors,
-                                         variables=feature_of_interest)
+# neighbors <- 10
+# locDiag_dalex_ind <- predict_diagnostics(explainer=exp_dalex,
+#                                          new_observation=sample,
+#                                          neighbors=neighbors,
+#                                          variables=feature_of_interest)
 
-plot(locDiag_dalex_ind)
+# plot(locDiag_dalex_ind)
 
-# Stop and report time
-locDiag_end <- Sys.time()
-locDiag_runtim <- difftime(locDiag_end, locDiag_start, units='mins')
-print(paste0('Runtime for local diagnostic plots: ', round(locDiag_runtime, 2), ' minutes'))
+# # Stop and report time
+# locDiag_end <- Sys.time()
+# locDiag_runtime <- difftime(locDiag_end, locDiag_start, units='mins')
+# print(paste0('Runtime for local diagnostic plots: ', round(locDiag_runtime, 2), ' minutes'))
 
 
 #############################
@@ -307,7 +324,7 @@ plot(vImp_dalex)
 
 # Stop and report time
 vImp_end <- Sys.time()
-vImp_runtim <- difftime(vImp_end, vImp_start, units='mins')
+vImp_runtime <- difftime(vImp_end, vImp_start, units='mins')
 print(paste0('Runtime variable importence plots: ', round(vImp_runtime, 2), ' minutes'))
 
 
@@ -325,11 +342,13 @@ pdp_start <- Sys.time()
 pdp_dalex <- model_profile(explainer=exp_dalex, 
                            variables=feature_of_interest)
 
-plot(pdp_dalex)                           
+if (overwrite) {png(paste0(result_filepath, 'pdp_', feature_of_interest, '.png'))}
+plot(pdp_dalex)  
+if (overwrite) {dev.off()}
 
 # Stop and report time
 pdp_end <- Sys.time()
-pdp_runtim <- difftime(pdp_end, pdp_start, units='mins')
+pdp_runtime <- difftime(pdp_end, pdp_start, units='mins')
 print(paste0('Runtime for PDP plots: ', round(pdp_runtime, 2), ' minutes'))
 
 
@@ -338,17 +357,17 @@ print(paste0('Runtime for PDP plots: ', round(pdp_runtime, 2), ' minutes'))
 # Start time measurement 
 gPdp_start <- Sys.time()
 
-feature_to_group <- 'x1' # Must be categorical variable 
-
 gPdp_dalex <- model_profile(explainer=exp_dalex, 
                            variables=feature_of_interest,
                            groups=feature_to_group)
 
-plot(gPdp_dalex)    
+if (overwrite) {png(paste0(result_filepath, 'pdp_', feature_of_interest, '_grouped_by_', feature_to_group, '.png'))}
+plot(gPdp_dalex)  
+if (overwrite) {dev.off()}
 
 # Stop and report time
 gPdp_end <- Sys.time()
-gPdp_runtim <- difftime(gPdp_end, gPdp_start, units='mins')
+gPdp_runtime <- difftime(gPdp_end, gPdp_start, units='mins')
 print(paste0('Runtime for grouped PDP plots: ', round(gPdp_runtime, 2), ' minutes'))
 
 #  Save the runtimes
