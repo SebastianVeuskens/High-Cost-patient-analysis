ca48b9d70075076a796036a7c5c71d4fae60668c
Sebastian Veuskens <sebastian.veuskens@googlemail.com>
2024-10-21 16:21:57 +0200

    Restructure folder order

--- code/1_data_preparation.R
+++ code/1_data_preparation.R
@@ -14,10 +14,6 @@
 ### MODIFY ####
 # Your working directory
 setwd("C:/Users/s.veuskens/Documents/Sebastian/Projekt Sebastian/modelling")
-# Indicates whether to include High-Cost patients from the last year into analysis 
-filter_hc <- FALSE 
-# Indicates whether to include as many High-Cost patients as not-High-Cost patients 
-balance_hc <- FALSE 
 # The training dataset is split into train and validate. Validate is used to compare different hyperparameters 
 proportion_train <- 0.75
 # The years that are used for training the models
@@ -29,9 +25,6 @@ exclude_cols <- c('year', 'ID')
 # Whether you want to save your results (and overwrite the old results) or not
 overwrite <- TRUE
 #### MODIFY END ####
-
-# Indicate from which relative location to load & save from. Depends on user input. 
-relative_dir <- paste0(ifelse(filter_hc, 'filtered/', 'complete/'), ifelse(balance_hc, 'balanced/', 'unbalanced/'))
 
 set.seed(12345)
 
@@ -45,34 +38,6 @@ data_2019$HC_Patient_Next_Year <- as.factor(data_2019$HC_Patient_Next_Year)
 data_2020$HC_Patient_Next_Year <- as.factor(data_2020$HC_Patient_Next_Year)
 data_2021$HC_Patient_Next_Year <- as.factor(data_2021$HC_Patient_Next_Year)
 data$HC_Patient_Next_Year <- as.factor(data$HC_Patient_Next_Year)
-
-# Filter out the (past) High-Cost patients, if specified 
-if (filter_hc) {
-    data <- data[data$HC_Patient == 0, ]
-}
-
-# Balance the data set, if specified
-# TODO: Either delete or adapt this part with data_2019... instead of just data 
-if (balance_hc) {
-    # The samples that will be in the final data set 
-    samples_included <- c()
-    # Indices of the High-Cost patients 
-    indices_hc <- data$HC_Patient_Next_Year == 1
-    # For each year, filter out all High-Cost patients and the same amount of not-High-Cost patients 
-    for (year in c(year_training, year_test)) {
-        indices_year        <- data$year == year
-        samples_hc          <- data[indices_hc & indices_year,]
-        all_samples_not_hc  <- data[(!indices_hc) & indices_year,]
-        indices_not_hc      <- sample(1:nrow(all_samples_not_hc), nrow(samples_hc))
-        samples_not_hc      <- all_samples_not_hc[indices_not_hc,]
-        # Add the High-Cost patients to the final balanced data set
-        samples_included    <- rbind(samples_included, samples_hc)
-        # Add the not-High-Cost patients to the final balanced data set
-        samples_included    <- rbind(samples_included, samples_not_hc)
-    }
-    # Continue working with the balanced data set 
-    data <- as.data.frame(samples_included)
-}
 
 # Assign train_validate, train, validate and test datasets 
 train_validate <- data[data$year %in% year_training, ]
@@ -103,23 +68,21 @@ dim(test)
 # Save the datasets 
 if (overwrite) {
     dir.create('data', showWarning=FALSE)
-    dir.create(paste0('data/', ifelse(filter_hc, 'filtered', 'complete')), showWarnings=FALSE)
-    dir.create(paste0('data/', relative_dir), showWarnings=FALSE)
-    save(data_2019,      file=paste0('data/', relative_dir, 'data_2019.Rdata'))
-    save(data_2020,      file=paste0('data/', relative_dir, 'data_2020.Rdata'))
-    save(data_2021,      file=paste0('data/', relative_dir, 'data_2021.Rdata'))
-    save(data,           file=paste0('data/', relative_dir, 'data.Rdata'))
-    save(train_validate, file=paste0('data/', relative_dir, 'train_validate.Rdata'))
-    save(train,          file=paste0('data/', relative_dir, 'train.Rdata'))
-    save(validate,       file=paste0('data/', relative_dir, 'validate.Rdata'))
-    save(test,           file=paste0('data/', relative_dir, 'test.Rdata'))
+    save(data_2019,      file='data/data_2019.Rdata')
+    save(data_2020,      file='data/data_2020.Rdata')
+    save(data_2021,      file='data/data_2021.Rdata')
+    save(data,           file='data/data.Rdata')
+    save(train_validate, file='data/train_validate.Rdata')
+    save(train,          file='data/train.Rdata')
+    save(validate,       file='data/validate.Rdata')
+    save(test,           file='data/test.Rdata')
 
-    write.csv(as.data.frame(data_2019),      file=paste0('data/', relative_dir, 'data_2019.csv'), row.names=FALSE)
-    write.csv(as.data.frame(data_2020),      file=paste0('data/', relative_dir, 'data_2020.csv'), row.names=FALSE)
-    write.csv(as.data.frame(data_2021),      file=paste0('data/', relative_dir, 'data_2021.csv'), row.names=FALSE)
-    write.csv(as.data.frame(data),           file=paste0('data/', relative_dir, 'data.csv'), row.names=FALSE)
-    write.csv(as.data.frame(train_validate), file=paste0('data/', relative_dir, 'train_validate.csv'), row.names=FALSE)
-    write.csv(as.data.frame(train),          file=paste0('data/', relative_dir, 'train.csv'), row.names=FALSE)
-    write.csv(as.data.frame(validate),       file=paste0('data/', relative_dir, 'validate.csv'), row.names=FALSE)
-    write.csv(as.data.frame(test),           file=paste0('data/', relative_dir, 'test.csv'), row.names=FALSE)
+    write.csv(as.data.frame(data_2019),      file='data/data_2019.csv', row.names=FALSE)
+    write.csv(as.data.frame(data_2020),      file='data/data_2020.csv', row.names=FALSE)
+    write.csv(as.data.frame(data_2021),      file='data/data_2021.csv', row.names=FALSE)
+    write.csv(as.data.frame(data),           file='data/data.csv', row.names=FALSE)
+    write.csv(as.data.frame(train_validate), file='data/train_validate.csv', row.names=FALSE)
+    write.csv(as.data.frame(train),          file='data/train.csv', row.names=FALSE)
+    write.csv(as.data.frame(validate),       file='data/validate.csv', row.names=FALSE)
+    write.csv(as.data.frame(test),           file='data/test.csv', row.names=FALSE)
 } 
